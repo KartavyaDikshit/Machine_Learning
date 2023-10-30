464c47b684a76150874e0491874f7957a62a2c2c
KartavyaDikshit <kartavyaniraj.dikshit2021@vitstudent.ac.in>
2023-10-30 11:58:44 +0530

    CodeExplained

--- Test_1.py
+++ Test_1.py
@@ -9,16 +9,28 @@ symbol = "AAPL"
 start_date = "2020-01-01"
 end_date = "2021-12-31"
 data = yf.download(symbol, start=start_date, end=end_date)
+print(data)
+
 
 # Data Preprocessing
 data = data[['Close']]
+print(data)
+
 data['Next_Close'] = data['Close'].shift(-1)
-data = data.dropna().copy()  # Make a copy to avoid SettingWithCopyWarning
+print(data)
+
+data = data.dropna() # Make a copy to avoid SettingWithCopyWarning
+print(data)
+
 
 # Splitting Data
 X = data[['Close']]
 y = data['Next_Close']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
 
 # Model Selection and Training
 model = LinearRegression()
@@ -26,6 +38,9 @@ model.fit(X_train, y_train)
 
 # Model Evaluation
 y_pred = model.predict(X_test)
+ans=model.score(X_test,y_test)
+print(ans)
+
 mse = mean_squared_error(y_test, y_pred)
 print(f"Mean Squared Error: {mse}")
 
