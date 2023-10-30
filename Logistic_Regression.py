ee8501aff87a7e8c04fe780336cb88a31a977060
KartavyaDikshit <kartavyaniraj.dikshit2021@vitstudent.ac.in>
2023-10-30 14:33:43 +0530

    Logistic_Regression

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -1,22 +1,48 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Load the dataset
-data = pd.read_csv('chat_dataset.csv', delimiter='\t')
+# Load the dataset from the CSV file
+df = pd.read_csv("chat_dataset.csv")
 
-# Check the column names in your dataset
-print(data.columns)
+# Convert sentiment labels to numerical values (positive: 1, neutral: 0, negative: -1)
+df['sentiment'] = df['sentiment'].map({'positive': 1, 'neutral': 0, 'negative': -1})
 
-# Based on the column names, select the appropriate columns
-X = data['message']  # Adjust this to the actual column name for messages in your dataset
-y = data['sentiment']
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(df['message'], df['sentiment'], test_size=0.2, random_state=42)
 
-# Create a bar chart for sentiment distribution
-sentiment_counts = y.value_counts()
-plt.figure(figsize=(8, 6))
-sentiment_counts.plot(kind='bar', color=['green', 'red', 'blue'])
-plt.title('Sentiment Distribution')
+# Vectorize the text data using CountVectorizer
+vectorizer = CountVectorizer()
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.transform(X_test)
+
+# Create and train the logistic regression model
+clf = LogisticRegression()
+clf.fit(X_train_vec, y_train)
+
+# Predict sentiments for the test data
+y_pred = clf.predict(X_test_vec)
+
+# Calculate accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy * 100:.2f}%")
+
+# Generate a confusion matrix
+conf_matrix = confusion_matrix(y_test, y_pred)
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues')
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.show()
+
+# Data Visualization
+sentiment_counts = df['sentiment'].value_counts()
+sns.barplot(x=sentiment_counts.index, y=sentiment_counts.values)
+plt.xticks(range(3), ['Negative', 'Neutral', 'Positive'])
 plt.xlabel('Sentiment')
 plt.ylabel('Count')
-plt.xticks(rotation=0)  # Rotate x-axis labels if needed
 plt.show()
