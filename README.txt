56746f475d3e75b46d8f99504606eb4d299c9976
KartavyaDikshit <kartavyaniraj.dikshit2021@vitstudent.ac.in>
2023-10-30 10:15:03 +0530

    Initial commit

---
+++ README.txt
@@ -0,0 +1,20 @@
+Stock price prediction project in Python using the pandas, numpy, and scikit-learn libraries
+Linear regression model to predict stock prices
+Steps:
+Data Collection
+Data Preprocessing
+Splitting Data
+Model Selection and Training
+Model Evaluation
+Model Testing
+The Mean Squared Error (MSE) provides a measure of the model's accuracy.
+
+yfinance is used to download historical stock price data from Yahoo Finance.
+pandas is used for data manipulation and analysis.
+train_test_split is used to split the data into training and testing sets.
+LinearRegression is an algorithm from scikit-learn used for linear regression modeling.
+mean_squared_error is used to calculate the Mean Squared Error, a measure of the model's performance.
+
+stock symbol "AAPL" for Apple Inc. store it in the DataFrame:data \
+
+
---
+++ Test_1.py
@@ -0,0 +1,34 @@
+import yfinance as yf
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Data Collection
+symbol = "AAPL" #"AAPL" for Apple Inc.
+start_date = "2020-01-01"
+end_date = "2021-12-31"
+data = yf.download(symbol, start=start_date, end=end_date)
+
+# Data Preprocessing
+data = data[['Close']]
+data['Next_Close'] = data['Close'].shift(-1)
+data = data.dropna().copy()  # Make a copy to avoid SettingWithCopyWarning
+
+# Splitting Data
+X = data[['Close']]
+y = data['Next_Close']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Model Selection and Training
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Model Evaluation
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error: {mse}")
+
+# Model Testing
+predicted_price = model.predict(X_test.tail(1).values.reshape(1, -1))
+print(f"Predicted Price for the Next Day: {predicted_price[0]:.2f}")
